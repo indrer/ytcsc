271e6bef415451641e4683e1f9b625eb90a5ed2c
Indre <hic.3t.nunc@gmail.com>
2021-03-03 15:06:34 +0100

    Add data fetcher

---
+++ data-fetcher.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import psycopg2
+from secret import database_connect
+from secret import query
+
+column_names = ['body', 'positive', 'negative', 'neutral', 'rated', 'comment_id', 'video_id', 'date']
+
+try:
+    connection = psycopg2.connect(**database_connect)
+    cur = connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    df = pd.DataFrame(data, columns=column_names)
+    df.to_csv('data/raw_data.csv', index=False)
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if connection is not None:
+        connection.close()
+        print('Database connection closed.')
+
