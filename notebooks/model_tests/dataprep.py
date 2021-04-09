e69dee1f33ae4a768dd733287acdd39b5c8821f0
unknown <hic.3t.nunc@gmail.com>
2021-04-09 19:21:25 +0200

    Add models without pretrained word vectors

--- notebooks/model_tests/dataprep.py
+++ notebooks/model_tests/dataprep.py
@@ -8,12 +8,21 @@ from gensim.models import Word2Vec, KeyedVectors
 
 seed = 42
 _word_vectors_file = '../../models/w2v/word2vec.wordvectors'
+_word_vectors_file_stem = '../../models/w2v/word2vec_stem.wordvectors'
+_word_vectors_file_long = '../../models/w2v/word2vec_long.wordvectors'
 
-def get_vectors():
-    return KeyedVectors.load(_word_vectors_file, mmap='r')
+def get_vectors(wv_type=0):
+    if wv_type == 0:
+        return KeyedVectors.load(_word_vectors_file, mmap='r')
+    elif wv_type == 1:
+        return KeyedVectors.load(_word_vectors_file_stem, mmap='r')
+    elif wv_type == 2:
+        return KeyedVectors.load(_word_vectors_file_long, mmap='r')
+    else:
+        print('0 - word vectors for not stemmed data,\n1 - word vectors for stemmed data,\n2 - word vectors for longer comments')
 
 
-def get_data(cmt_pos, cmt_neg):
+def get_data(cmt_pos, cmt_neg, wv_type):
     # Drop random negative indices
     negative_indices = cmt_neg.index.tolist()
     diff = abs(len(cmt_neg) - len(cmt_pos))
@@ -22,7 +31,7 @@ def get_data(cmt_pos, cmt_neg):
     df = pd.concat([cmt_pos, cmt_neg], ignore_index=True)
     
     # Get the length of vectors
-    vectors = get_vectors()
+    vectors = get_vectors(wv_type)
     vocab_size = len(vectors.vocab)
     emdedding_size = vectors.vector_size
     del vectors
