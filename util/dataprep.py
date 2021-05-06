d641c4f850bdc69c5ea4d109f0834665e59265fe
unknown <hic.3t.nunc@gmail.com>
2021-05-07 00:29:59 +0200

    Update tests with final data

--- util/dataprep.py
+++ util/dataprep.py
@@ -8,10 +8,10 @@ from gensim.models import Word2Vec, KeyedVectors
 
 
 seed = 42
-_word_vectors_file = '../../models/w2v/word2vec.wordvectors'
-_word_vectors_file_stem = '../../models/w2v/word2vec_stem.wordvectors'
-_word_vectors_file_long = '../../models/w2v/word2vec_long.wordvectors'
 _glove_vectors = '../../glove.twitter.27B.200d.txt'
+_train_path = '../../datasets/split/train.csv'
+_test_path = '../../datasets/split/test.csv'
+_val_path ='../../datasets/split/val.csv'
 
 def get_glove_embeddings(word_index):
     glove_vectors = open(os.path.join(_glove_vectors), encoding="utf8")
@@ -30,27 +30,14 @@ def get_glove_embeddings(word_index):
             embedding_matrix[i] = embedding_vector
     return embedding_matrix
 
-def get_vectors(wv_type=0, word_index=None):
-    if wv_type == 0:
-        return KeyedVectors.load(_word_vectors_file, mmap='r')
-    elif wv_type == 1:
-        return KeyedVectors.load(_word_vectors_file_stem, mmap='r')
-    elif wv_type == 2:
-        return KeyedVectors.load(_word_vectors_file_long, mmap='r')
-    elif wv_type == 3:
-        return get_glove_embeddings(word_index)
-    else:
-        print('0 - word vectors for not stemmed data,\n1 - word vectors for stemmed data,\n2 - word vectors for longer comments')
+def get_vectors(word_index=None):
+    return get_glove_embeddings(word_index)
 
-
-def get_data(cmt_pos, cmt_neg, wv_type):
-    # Drop random negative indices
-    negative_indices = cmt_neg.index.tolist()
-    diff = abs(len(cmt_neg) - len(cmt_pos))
-    indices = np.random.choice(negative_indices, diff, replace=False)
-    cmt_neg = cmt_neg.drop(indices)
-    df = pd.concat([cmt_pos, cmt_neg], ignore_index=True)
-    
+def get_data():
+    train = pd.read_csv(_train_path)
+    test = pd.read_csv(_test_path)
+    val = pd.read_csv(_val_path)
+    df = pd.concat([train, test, val], ignore_index=True)
     sentences = [[word for word in str(body).split()] for body in df.body]
 
     #Set up tokenizer
@@ -59,19 +46,28 @@ def get_data(cmt_pos, cmt_neg, wv_type):
     word_index = tokenizer.word_index
 
     # Get the length of vectors
-    vectors = get_vectors(wv_type, word_index)
-    if wv_type == 3:
-        vocab_size = len(word_index) + 1
-        emdedding_size = 200
-    else:
-        vocab_size = len(vectors.vocab)
-        emdedding_size = vectors.vector_size
-        vectors = vectors.vectors
+    vectors = get_vectors(word_index)
+    vocab_size = len(word_index) + 1
+    emdedding_size = 200
 
+    # Make data into tokens
     X = tokenizer.texts_to_sequences(sentences)
     X = sequence.pad_sequences(X)
-    Y= df.rating.values
-    return X, Y, vocab_size, emdedding_size, vectors
+
+    # Prepare train data
+    X_train = X[0:len(train)]
+    y_train = train.rating.values
+
+    # Prepare test data
+    test_index = (len(train)) + (len(test))
+    X_test = X[len(train):test_index]
+    y_test = test.rating.values
+
+    # Prepare val data
+    X_val = X[test_index:]
+    y_val = val.rating.values
+
+    return X_train, y_train, X_test, y_test, X_val, y_val, vocab_size, emdedding_size, vectors
 
 
 
